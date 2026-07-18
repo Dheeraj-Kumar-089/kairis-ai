4b581d3fd494f3a6d1b2c57e4e4f0951277a93b4
Dheeraj <dheerajkp810@gmail.com>
2026-07-18 21:37:34 +0530

    clean: remove temporary index-dropping code

--- Backend/src/config/database.js
+++ Backend/src/config/database.js
@@ -3,18 +3,6 @@ import mongoose from "mongoose";
 
 const connectToDB = async () => {
     const conn = await mongoose.connect(process.env.MONGO_URI);
-    console.log("Connected to DB ");
-
-    try {
-        const usersCollection = mongoose.connection.collection('users');
-        const indexes = await usersCollection.indexes();
-        const hasUsernameIndex = indexes.some(idx => idx.name === 'username_1');
-        if (hasUsernameIndex) {
-            await usersCollection.dropIndex('username_1');
-            console.log("Successfully dropped obsolete 'username_1' index.");
-        }
-    } catch (err) {
-        console.warn("Could not drop username_1 index (it might not exist or connection was not ready):", err.message);
-    }
+    console.log("Connected to DB ")
 }
 export default connectToDB;
